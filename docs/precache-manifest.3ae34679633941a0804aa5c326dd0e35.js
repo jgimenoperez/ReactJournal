518d200f079214dbd27322aacb606fd85a991dc2
jgimenoperez <jgimenoperez@gmail.com>
2021-07-21 19:44:59 +0200

    subo docs

---
+++ docs/precache-manifest.3ae34679633941a0804aa5c326dd0e35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0e55323651daf12974057b44388fcac",
+    "url": "/ReactJournal/index.html"
+  },
+  {
+    "revision": "891eea7e9341a1420406",
+    "url": "/ReactJournal/static/css/main.26922b4e.chunk.css"
+  },
+  {
+    "revision": "3d8c52d0d8c60d78b271",
+    "url": "/ReactJournal/static/js/2.235a95a9.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/ReactJournal/static/js/2.235a95a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "891eea7e9341a1420406",
+    "url": "/ReactJournal/static/js/main.95c899c6.chunk.js"
+  },
+  {
+    "revision": "482258f16c04a94e7951",
+    "url": "/ReactJournal/static/js/runtime-main.aeb68955.js"
+  }
+]);
